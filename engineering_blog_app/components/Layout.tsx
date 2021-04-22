09db73d1421cc9924c31df778c5d634ef0e21c49
CRTW26 <worthington.connor@gmail.com>
2021-04-22 15:57:13 +0100

    Adds header styling

--- engineering_blog_app/components/Layout.tsx
+++ engineering_blog_app/components/Layout.tsx
@@ -1,10 +1,12 @@
 import styles from '../styles/Layout.module.css';
 import Nav from '../components/Navbar';
+import Header from '../components/Header'
 
 const Layout = ({children}) => {
   return (
     <>
       <Nav />
+      <Header />
       <div className={styles.container}>
         <main className={styles.main}>
           {children}
