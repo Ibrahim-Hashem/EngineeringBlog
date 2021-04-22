20f99a3c5b770b998f97b20b35611701a7cc8db2
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-04-22 11:52:54 +0100

    navbar component created

---
+++ engineering_blog_app/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    <h1>hello world</h1>
+    </>
+  )
+}
