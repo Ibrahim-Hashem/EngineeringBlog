79205cc51796f6a5498e56d8ccaf129cd0b1fd36
CRTW26 <worthington.connor@gmail.com>
2021-04-23 14:34:01 +0100

    Add article to id page

--- engineering_blog_app/pages/[Aid].tsx
+++ engineering_blog_app/pages/[Aid].tsx
@@ -1,12 +1,23 @@
 import {useRouter} from 'next/router'
 
-const Article = ({article})=>{
+const Article = ({articles})=>{
     const router = useRouter()
-    const { Aid } = router.query;
-    console.log(Aid)
-    return <h1>{Aid}</h1>
+    const { Aid }= router.query;
+    const article = articles.filter(article => article.id.toString() === Aid)
+    
+    return (
+      <div> 
+        <h1>{article[0].title}</h1>
+        <p>{article[0].body}</p>
+      </div>
+    )
 }
 
+export const getServerSideProps = async () => {
+  const res = await fetch('http://localhost:3000/api/posts');
+  const articles = await res.json();
+  return {props: {articles}}
+}
 
 
 export default Article
