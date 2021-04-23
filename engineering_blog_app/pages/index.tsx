aaa325a24fcdc66b83045122c0fadc266fe475aa
CRTW26 <worthington.connor@gmail.com>
2021-04-23 11:51:58 +0100

    Add interface file structure with articleInterface

--- engineering_blog_app/pages/index.tsx
+++ engineering_blog_app/pages/index.tsx
@@ -19,7 +19,6 @@ export default function Home({articles}) {
 export const getStaticProps: GetStaticProps = async () => {
   const res = await fetch('http://localhost:3000/api/posts');
   const articles = await res.json();
-  console.log(res);
 
   return {
     props: {
