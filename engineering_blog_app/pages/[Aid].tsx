a80d241573732757f968ef41f54bd2a2b50580ed
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-04-23 14:38:57 +0100

    bug fix

--- engineering_blog_app/pages/[Aid].tsx
+++ engineering_blog_app/pages/[Aid].tsx
@@ -3,8 +3,7 @@ import {useRouter} from 'next/router'
 const Article = ({articles})=>{
     const router = useRouter()
     const { Aid }= router.query;
-    const article = articles.filter(article => article.id.toString() === Aid)
-    
+    const article = articles.filter(article => article.id.toString() === Aid);
     return (
       <div> 
         <h1>{article[0].title}</h1>
