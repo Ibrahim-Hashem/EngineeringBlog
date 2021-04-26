93fb9c3c6a8215e3e5fc595bca5f2fa726765981
CRTW26 <worthington.connor@gmail.com>
2021-04-26 14:35:26 +0100

    Apply existing styles to BlogCard

--- engineering_blog_app/components/BlogCard.tsx
+++ engineering_blog_app/components/BlogCard.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/Link';
 
+import styles from '../styles/Article.module.css';
+
 const BlogCard = ({blogToShow}) => {
   return (
-        <Link href={`/${blogToShow.slug}`}>
-          <a>
-            <h1>{blogToShow.title}</h1>
-            <p>{blogToShow.author}</p>
-            <p>{blogToShow.date}</p>
-          </a>
-        </Link>
+    <div className={styles.article_card}>
+      <Link href={`/${blogToShow.slug}`}>
+        <a>
+          <h1>{blogToShow.title}</h1>
+          <p>{blogToShow.author}</p>
+          <p>{blogToShow.date}</p>
+        </a>
+      </Link>
+    </div>
   )
 }
 
