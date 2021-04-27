ae0cb1f8072389c52b688a6f78c11981c370d83a
CRTW26 <worthington.connor@gmail.com>
2021-04-27 08:35:05 +0100

    Remove comments from [slug] file

--- engineering_blog_app/pages/[slug].tsx
+++ engineering_blog_app/pages/[slug].tsx
@@ -7,50 +7,26 @@ import html from 'remark-html';
 import markdownParser from 'remark-parse';
 
 const Article = ({blog})=>{
-  // const router = useRouter()
-  console.log(blog)
-  // const { slug } = router.query;
-  // console.log("###########")
-  // console.log(slug)
-  // console.log("###########")
-    // const blog = blogs.filter(b => b.slug === slug);
     return (
-      // <div className={Style.main}> 
-      //   <h1>{blog[0].title}</h1>
-      //   <p><span>{blog[0].author}</span></p>
-      //   <p>{blog[0].date}</p>
-      // </div>
        <div>
          <h1>{blog.title}</h1>
          <p>{blog.author}</p>
          <p>{blog.date}</p>
-         {/* <p>{blog.body}</p> */}
          <section dangerouslySetInnerHTML={{ __html: blog.body }}></section>
        </div>
     )
 }
 
-// export const getServerSideProps = async () => {
-//   const res = await fetch('http://localhost:3000/api/posts');
-//   const articles = await res.json();
-//   return {props: {articles}}
-// }
 
 export async function getStaticProps(context) {
-  const files = fs.readdirSync(`${process.cwd()}/blogs`);
 
-  const blogs = files
-    // const filePath = `${process.cwd()}/blogs/${file}`;
     const blog = fs.readFileSync(`${process.cwd()}/blogs/${context.params.slug}.md`, 'utf-8')
     const {content, data} = grayMatter(blog)
-    // console.log(content)
-    // console.log(data)
     const markdownOutput = await unified()
       .use(markdownParser)
       .use(html)
       .process(content)
 
-      console.log(markdownOutput);
   return {
     props: {
       blog: {
@@ -67,15 +43,11 @@ export async function getStaticPaths(){
     .map((file) => {
     return {params: {slug: file.replace(".md", "")} }
   })
-  // console.log("###########")
-  // console.log(blogs)
-  // console.log("###########")
 
   return{
     paths: blogs,
     fallback: false
   }
 }
-
 
 export default Article
